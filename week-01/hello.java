1f8bfe2ed389bd1705b1d419256cff90fd87202d
Rustam-Z <zokirovrustam202@gmail.com>
2020-10-09 11:50:08 +0500

    first commmit

---
+++ week-01/hello.java
@@ -0,0 +1,9 @@
+// Your First Program
+import java.util.Scanner;
+class main {
+    public static void main(String[] args) {
+       // Scanner id = new Scanner(System.in);
+       //System.out.println(id.nextLine());
+	System.out.println("Hello Rustam!");
+    }
+}
---
+++ week-01/src/main.java
@@ -0,0 +1,13 @@
+// Your First Program
+import java.util.Scanner;
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println(5/2.0);
+
+        Scanner $id = new Scanner(System.in);
+        System.out.println($id.next());
+
+
+    }
+}
